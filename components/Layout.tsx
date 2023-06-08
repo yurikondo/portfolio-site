ee349991b9f2d50162d22f6837fae1a12288a1be
yuri <yuri@example.com>
2023-06-09 07:22:06 +0900

    Begin a project

---
+++ components/Layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return <div>{children}</div>;
+};
+
+export default Layout;
