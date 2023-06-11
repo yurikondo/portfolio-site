9786978eb3480a77eec763aa66e1109943bf926b
yuri <yuri@example.com>
2023-06-11 12:43:24 +0900

    Skillsセクションを追加

---
+++ src/components/skill/Skills.tsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import styles from "../../styles/Home.module.css";
+
+export class Skills extends Component {
+  render() {
+    return (
+      <div
+        id="skills"
+        className="container mt-64 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full z-20"
+      >
+        <section className="w-full">
+          <h2>スキルスタック</h2>
+          <p>私は以下のプログラミング言語を取り扱ってきました</p>
+
+          <div className="my-16 ">
+            <div className="w-full border border-gray-300 p-16 lg:px-32 lg:py-20 flex justify-center lg:justify-start flex-wrap lg:space-x-32 hover:border-yellow-300 transition-all duration-300 cursor-pointer">
+              <div className="mb-6 lg:mb-0 self-center">
+                <i className="fa-brands fa-square-js text-9xl text-yellow-300"></i>
+              </div>
+
+              <div className="text-center flex flex-wrap justify-center lg:text-left lg:block">
+                <h3 className="text-3xl font-semibold">Javascript</h3>
+                <div className="flex flex-wrap w-full justify-center gap-3 lg:w-auto lg:justify-start mt-6 mb-8">
+                  <div className={styles.badge}>ウェブ制作</div>
+                  <div className={styles.badge}>フロントエンド制作</div>
+                </div>
+                <p>
+                  フロントエンドエンジニア。普段はReactを使って開発しています
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Skills;
