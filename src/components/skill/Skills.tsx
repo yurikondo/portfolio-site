4a7ec8a4ebb17cd68c3c86306d29b40601e2abe6
yuri <yuri@example.com>
2023-06-23 08:20:11 +0900

    Skillsセクションの文言を変更

--- src/components/skill/Skills.tsx
+++ src/components/skill/Skills.tsx
@@ -7,24 +7,23 @@ const skills: any = [
     id: "Javascript",
     icon: "fa-square-js",
     color: "yellow-300",
-    tags: ["ウェブ制作", "フロントエンド制作"],
-    desc: "フロントエンドエンジニア。普段はReactを使って開発しています",
+    tags: ["ウェブ制作", "バックエンド"],
+    desc: "1番好きな言語はJavascriptです。",
   },
   {
     id: "React",
     icon: "fa-react",
     color: "sky-400",
-    tags: ["ウェブ制作", "フロントエンド制作"],
-    desc: "フロントエンドエンジニア。普段はReactを使って開発しています",
+    tags: ["Next.js", "フロントエンド制作"],
+    desc: "このポートフォリオサイトは、ReactのフレームワークのNext.jsを使って作成しました。",
   },
   {
     id: "Node.js",
     icon: "fa-node",
     color: "lime-400",
-    tags: ["ウェブ制作", "フロントエンド制作"],
-    desc: "フロントエンドエンジニア。普段はReactを使って開発しています",
+    tags: ["Express", "Api開発"],
+    desc: "ポートフォリオアプリのバックエンドは、Node.jsのフレームワークのExpressを使って作成しました。",
   },
-
 ];
 
 export class Skills extends Component {
@@ -40,7 +39,7 @@ export class Skills extends Component {
             desc="私は以下のプログラミング言語を取り扱ってきました"
           />
           {skills.map((skill: any) => (
-            <div className="my-16 ">
+            <div className="my-16 " key={skill.id}>
               <div
                 className={`w-full border border-gray-300 p-16 lg:px-32 lg:py-20 flex justify-center lg:justify-start flex-wrap lg:space-x-32 hover:border-${skill.color} transition-all duration-300 cursor-pointer`}
               >
