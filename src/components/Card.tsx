56356aa6e2f68c4631c09d60e7d3c894902e45ea
yuri <yuri@example.com>
2023-06-23 19:45:05 +0900

    スマホの画像を追加

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import VanillaTilt from "vanilla-tilt";
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Card = () => {
   return (
     <div
-      className={`border-solid border-2 border-sky-500 rounded-xl shadow-lg w-80 h-96 bg-black absolute top-64 z-10 ${styles.phone}`}
+      className={`border-solid border-2 border-sky-500 rounded-xl shadow-lg w-80 h-96 bg-black absolute top-60 z-10 ${styles.phone}`}
       data-tilt
       // data-tilt-glare="true"
       // data-tilt-max="1"
     >
-      Card
+      <Image
+        alt="スマホの画像"
+        src="/imgs/hero/screenshot.png"
+        width={300}
+        height={150}
+      />
     </div>
   );
 };
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import styles from "../../styles/Home.module.css";
-import Image from "next/image";
 import Card from "../Card";
 
 interface RotateItem {
