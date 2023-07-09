5810256b2e97a8a66facea0b1187d960e9c4286a
yuri <yuri@example.com>
2023-07-09 16:26:55 +0900

    ポートフォリオアプリへ遷移するボタンを追加

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,12 +11,26 @@ const Card = () => {
       // data-tilt-glare="true"
       // data-tilt-max="1"
     >
-      <Image
-        alt="スマホの画像"
-        src="/imgs/hero/phoneImg.png"
-        width={250}
-        height={130}
-      />
+      <div className="relative flex justify-center">
+        <Image
+          alt="スマホの画像"
+          src="/imgs/hero/phoneImg.png"
+          width={250}
+          height={130}
+        />
+        <button
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full absolute bottom-5"
+          onClick={() => {
+            window.open(
+              "https://amapost-5d413f7c5092.herokuapp.com/",
+              "_blank"
+            );
+          }}
+          rel="noopener noreferrer"
+        >
+          アプリを開く
+        </button>
+      </div>
     </div>
   );
 };
